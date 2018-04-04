7c8c502f8fd8cc6a43f18e83e45c392a8d16f3a8
Matthew Gasiewski <matthewjames84@gmail.com>
2018-04-03 20:32:49 -0400

    some template work

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,15 @@
 
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Task
 
 def index(request):
     tasks_list = Task.objects.all()
-    template = loader.get_template('index.html')
     context = {
         'tasks_list': tasks_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 def detail(request, task_id):
-    response = "Task %s: "
-    return HttpResponse(response % task_id)
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'detail.html', {'task': task})

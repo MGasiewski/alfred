100d55ec0526db8299b7bff1494035cbb7607d22
Matthew Gasiewski <matthewjames84@gmail.com>
2018-04-27 20:50:56 -0400

    trying to fix login

--- Butler/urls.py
+++ Butler/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contribu.auth import views
 
 urlpatterns = [
     path('tasks/', include('tasks.urls')),
     path('shopping/', include('shopping.urls')),
     path('admin/', admin.site.urls),
-    path('',  auth_views.login, name='login'),
+    path('',  views.LoginView.as_view(), name="login"),
     path('accounts/', include('django.contrib.auth.urls')),
 ]

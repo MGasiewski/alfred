7e5e9bfbc21ed28c4d3b1d7a627c64e3b6939afc
Matthew Gasiewski <matthewjames84@gmail.com>
2018-04-27 20:53:00 -0400

    x

--- Butler/urls.py
+++ Butler/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contribu.auth import views
+from django.contrib.auth import views
 
 urlpatterns = [
     path('tasks/', include('tasks.urls')),
